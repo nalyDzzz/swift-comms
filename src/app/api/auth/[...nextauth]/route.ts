1f0f3f3d065715c11fd7dacc580087b787eeb32c
Dylan <dylanmarin2018@gmail.com>
2024-10-01 22:49:23 -0600

    added nextauth for authentication

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+const handler = NextAuth({
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+    }),
+  ],
+  callbacks: {
+    async signIn({ user }) {
+      const { email, name } = user;
+      if (!email || !name) return false;
+      //   await addUserToDb(email, name);
+      return true;
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
